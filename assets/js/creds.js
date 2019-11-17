f4d1afd959fe55f6a6bc4807f8095c6b1e9921af
Michael Hrivnak <mhrivnak@gmail.com>
2019-11-16 22:46:14 -0500

    filled out sections

---
+++ assets/js/creds.js
@@ -0,0 +1,6 @@
+
+var gmailTO = "mdhrivnak@gmail.com";
+var secToken = "7e213f23-bac5-4877-869f-6af1c7ef5049";
+var secTokenb = "91e92157-128b-4dc3-9a13-b3fe50563c44";
+var gmailACC = "mhrivnak.smtp@gmail.com";
+var gmailPW = "4Mj-fr+pJ7gDwJg+";
