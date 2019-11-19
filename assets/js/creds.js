09c72d932a10bf654b67899172fa8d60de0a045a
Michael Hrivnak <mhrivnak@gmail.com>
2019-11-19 12:44:27 -0500

    updating token for smtp

--- assets/js/creds.js
+++ assets/js/creds.js
@@ -1,6 +1,6 @@
 
 var gmailTO = "mdhrivnak@gmail.com";
 var secToken = "7e213f23-bac5-4877-869f-6af1c7ef5049";
-var secTokenb = "91e92157-128b-4dc3-9a13-b3fe50563c44";
+var secTokenb = "b61404c1-962a-4117-abe6-9e775f44cecb";
 var gmailACC;
 var gmailPW;
