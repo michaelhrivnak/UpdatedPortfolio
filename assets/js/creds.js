d96bc8ab3aa74f01fa58a4db27ba1df84426903c
Michael Hrivnak <mhrivnak@gmail.com>
2019-11-19 18:25:17 -0500

    trying another key

--- assets/js/creds.js
+++ assets/js/creds.js
@@ -1,6 +1,6 @@
 
 var gmailTO = "mdhrivnak@gmail.com";
 var secToken = "7e213f23-bac5-4877-869f-6af1c7ef5049";
-var secTokenb = "b61404c1-962a-4117-abe6-9e775f44cecb";
+var secTokenb = "da3c502b-12bf-4170-9724-e3a148d4d06c";
 var gmailACC;
 var gmailPW;
